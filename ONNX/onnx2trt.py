f9ff581365c10c25adf19b054e28d354d16c58a1
Luis <542286191@qq.com>
2024-01-12 15:54:25 +0800

    无需pytorch的部分全部注释了

--- ONNX/onnx2trt.py
+++ ONNX/onnx2trt.py
@@ -4,8 +4,8 @@ import os
 import numpy as np
 import onnx
 
-import torch
-from torch.nn import functional as F
+# import torch
+# from torch.nn import functional as F
 import numpy as np
 import os
 import argparse
